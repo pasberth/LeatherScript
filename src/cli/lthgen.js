0e5391c55a26ae80bb30186714d07e842fb55674
pasberth <pasberth@gmail.com>
2014-04-24 18:34:40 +0900

    m

--- src/cli/lthgen.js
+++ src/cli/lthgen.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var tokenizer = require('./../tokenizer');
 var generator = require('./../generator');
+var typing = require('./../typing');
 var escodegen = require('escodegen');
 
 var path = process.argv[2];
@@ -12,5 +13,17 @@ fs.readFile(path, 'utf8', function (err1, text1) {
   }
 
   var lth_json = JSON.parse(text1);
+  var err = typing.typing(lth_json,
+                          { console:
+                            { variant:
+                              { tag: "log",
+                                val: { arrow: [
+                                  { simple: "string" } ,
+                                  { simple: "unit" }  ]}}}});
+  if (err) {
+    console.log(err);
+  }
+  console.log(JSON.stringify(lth_json, null, 2));
+
   console.log(escodegen.generate(generator.generate(lth_json)));
 });
